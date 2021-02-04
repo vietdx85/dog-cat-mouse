897f114a396fba9dcab4392d86f96e12b364ff33
vietdx85 <vietdx85@gmail.com>
2021-02-04 09:09:42 +0700

    Improve cat eat mothod

--- Cat.js
+++ Cat.js
@@ -1,5 +1,16 @@
+var Mouse = require('./Mouse');
+
 function Cat() {
 	this.stomach = [];
+}
+
+Cat.prototype.eat = function(animal) {
+	if(animal instanceof Mouse) {
+		this.stomach.push(animal);
+	} else {
+		throw new Error('Cat can only eat mouse!');
+	}
+
 }
 
 module.exports = Cat;
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 var Cat = require('./Cat');
+var Mouse = require('./Mouse');
+var Dog = require('./Dog');
+
+var cat = new Cat();
+var mouse = new Mouse('Mickey');
+var dog = new Dog();
+
+try {
+	cat.eat(dog);
+} catch (error) {
+	console.log('Error while cat eating a dog');
+}
+
+console.log(cat);
